f9b02b1bc5dccf8d14e2759ec3e7c06c79bc1f1b
Veronika Komarova <szdmcrew@yandex.ru>
2021-06-02 11:54:47 +0300

    turned on api, added video to the background

--- src/services/gotSevice.js
+++ src/services/gotSevice.js
@@ -1,17 +1,17 @@
-export default class GotService {
-	constructor(){
-		this._apiBase = 'http://www.omdbapi.com/';
-	}
-	getResource = async (url) => {
-		const res = await fetch(`${this._apiBase}${url}`);
-		if(!res.ok){
-			throw new Error(`Could not fetch ${url}, status: ${res.status}`);
-		}
-		return await res.json();
-	};
-	getAllMovies = async () => {
-		// console.log(this); обязательно!! в this будет GotService
-		let res = await this.getResource('?s=[title]&apikey=35b223ad')
-		return res.map(item => item)
-	}
-}
+// export default class GotService {
+// 	constructor(){
+// 		this._apiBase = 'http://www.omdbapi.com/';
+// 	}
+// 	getResource = async (url) => {
+// 		const res = await fetch(`${this._apiBase}${url}`);
+// 		if(!res.ok){
+// 			throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+// 		}
+// 		return await res.json();
+// 	};
+// 	getAllMovies = async () => {
+// 		// console.log(this); обязательно!! в this будет GotService
+// 		let res = await this.getResource('?s=[title]&apikey=35b223ad')
+// 		return res.map(item => item)
+// 	}
+// }
