eb95f26d2ca60b8dbfc2c16a742f4ddc169b901c
Veronika Komarova <szdmcrew@yandex.ru>
2021-07-02 23:27:16 +0300

    added slider and transition to detailed information through a card with a similar film

--- src/components/Similar/Similar.js
+++ src/components/Similar/Similar.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const Similar = ({similar}) => {
+	
+
 	return(
 		<div className="similar" style={{ backgroundImage: `url(${similar.image})` }}>
-			<div className="info infoSimilarSize">
-				<h5 className="similarTitle ">{similar.title}</h5>
-		  	<p className="similarItem">{similar.genres}</p>
-		  	<p className="similarDescription">{similar.plot}</p>
-		  	<div className="rating ">
-  			<p>
-  				<span className="textButtonPosition">IMDb</span><span>{similar.imDbRating}</span>
-  			</p>
-  		</div>
-			</div>
+				<Link to={`/movies/${similar.id}`}>
+					<div className="info infoSimilarSize">
+						<h5 className="similarTitle ">{similar.title}</h5>
+				  	<p className="similarItem">{similar.genres}</p>
+				  	<p className="similarDescription">{similar.plot}</p>
+				  	<div className="rating ">
+		  			<p>
+		  				<span className="textButtonPosition">IMDb</span><span>{similar.imDbRating}</span>
+		  			</p>
+		  		</div>
+				</div>
+			</Link>
 		</div>
 	)
 }
